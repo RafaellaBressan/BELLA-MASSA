59c17130ec133bff5b906bf98dd5a2dec04d1407
Rafaella Bressan <rafaellabressandacosta14@gmail.com>
2026-09-01 15:30:31 -0300

    Atualização do site Bella Massa

---
+++ README.md
@@ -0,0 +1,24 @@
+# ☕ Bella Massa
+
+Site institucional de uma cafeteria artesanal desenvolvido com HTML, CSS e JavaScript.
+
+## Tecnologias
+
+- HTML5
+- CSS3
+- JavaScript
+- Google Fonts
+
+## Funcionalidades
+
+- Design responsivo
+- Cardápio premium
+- Galeria com Lightbox
+- Depoimentos de clientes
+- Botão de WhatsApp
+- Rolagem suave
+- Layout moderno
+
+## Preview
+
+Projeto criado para compor meu portfólio de desenvolvimento web.
--- js/script.js
+++ js/script.js
@@ -19,3 +19,35 @@ behavior:"smooth"
 })
 
 })
+
+const modal=document.getElementById("modal");
+
+const modalImg=document.getElementById("modalImg");
+
+document.querySelectorAll(".lightbox").forEach(img=>{
+
+img.onclick=()=>{
+
+modal.style.display="flex";
+
+modalImg.src=img.src;
+
+}
+
+})
+
+document.querySelector(".close").onclick=()=>{
+
+modal.style.display="none";
+
+}
+
+modal.onclick=e=>{
+
+if(e.target===modal){
+
+modal.style.display="none";
+
+}
+
+}
